140838cd913673cc53fd1bd4315921dad198dcaf
Kirill Ershov <kirill.ersov@yandex.ru>
2023-03-03 04:57:02 +0300

    hw 4

---
+++ client/index.js
@@ -0,0 +1,54 @@
+const Web3 = require("web3");
+
+// Loading the contract ABI
+// (the results of a previous compilation step)
+const fs = require("fs");
+const { abi } = JSON.parse(fs.readFileSync("Token.json"));
+
+async function main() {
+  // Configuring the connection to an Ethereum node
+  const network = process.env.ETHEREUM_NETWORK;
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
+    )
+  );
+  // Creating a signing account from a private key
+  const signer = web3.eth.accounts.privateKeyToAccount(
+    process.env.PRIVATE_KEY
+  );
+  web3.eth.accounts.wallet.add(signer);
+  // Creating a Contract instance
+  const contract = new web3.eth.Contract(
+    abi,
+    process.env.CONTRACT_ADDRESS
+  );
+
+  if (process.argv[2] == "list") {
+    console.log(await contract.getPastEvents(process.argv[3], { fromBlock: 0 }));
+    process.exit(0);
+  }
+
+  var tx;
+  if (process.argv[2] == "add") {
+    tx = contract.methods.addMessage(process.argv[3])
+  } else if (process.argv[2] == "delete") {
+    tx = contract.methods.addMessage(process.argv[3])
+  }
+
+  const receipt = await tx
+    .send({
+      from: signer.address,
+      gas: await tx.estimateGas(),
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(`Mining transaction ...`);
+      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+    });
+
+  // The transaction is now on chain!
+  console.log(`Mined in block ${receipt.blockNumber}`);
+}
+
+require("dotenv").config();
+main();
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,4 +10,31 @@ contract Token is ERC20 {
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
+
+    struct Message {
+        uint256 id;
+        address sender;
+        string text;
+    }
+
+    mapping(uint256 => Message) messages;
+
+    event MessageAdded(uint256 id, address user, string text);
+    event MessageDeleted(uint256 id, address user);
+
+    uint256 private curId = 0;
+
+    function addMessage(string memory text) public {
+        messages[curId] = Message(curId, msg.sender, text);
+        emit MessageAdded(curId++, msg.sender, text);
+    }
+
+    function deleteMessage(uint256 id) public {
+       require(
+            messages[id].sender == msg.sender,
+            "user can only delete his own messages"
+        );
+        delete messages[id];
+        emit MessageDeleted(id, msg.sender);
+    }
 }
